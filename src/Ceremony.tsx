c47dd0bcc6946dafaa7e93ad8fd4a555d446042d
chris wedgwood <chriswedgwood@tfl.gov.uk>
2019-02-21 17:11:40 +0000

    ceremony

---
+++ src/Ceremony.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface CeremonyProps {
+}
+
+export default class Ceremony extends React.Component<CeremonyProps, any> {
+  public render() {
+    return (
+      <div>
+        THIS IS THE CEREMONY TEXT
+      </div>
+    );
+  }
+}
--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import "url-search-params-polyfill";
 import logo from "./logo.png";
-import {Nav} from "react-bootstrap"
+import {Navbar, Nav , NavDropdown} from "react-bootstrap"
 
 
 const Header: React.FC<RouteComponentProps> = props => {
@@ -27,25 +27,23 @@ const Header: React.FC<RouteComponentProps> = props => {
       
       <img src={logo} className="header-logo" alt="logo" />
       
-      <Nav
-  activeKey="/home"
-  
->
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>;
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse>
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>;
     </header>
   );
 };
